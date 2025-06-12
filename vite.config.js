01cdaa7a220f904d5437bbee45006486d9eb6c3c
Николай Дуенко <nikolajduenko@MacBook-Air-Nikolaj.local>
2025-06-13 00:23:59 +0300

    📦 Добавлен site.webmanifest и мета-теги для SEO

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    assetsInlineLimit: 0, // отключает инлайнинг больших файлов
+    assetsInlineLimit: 0,
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name]-[hash][extname]', // добавляет хеш к файлам
+        assetFileNames: 'assets/[name]-[hash][extname]',
         chunkFileNames: 'assets/[name]-[hash].js',
         entryFileNames: 'assets/[name]-[hash].js',
       },
+    },
+  },
+  server: {
+    headers: {
+      'Cache-Control': 'no-store',
     },
   },
 });
