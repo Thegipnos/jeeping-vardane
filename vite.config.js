636ae574e629bdd656ce5d063f419e296c1130fe
Николай Дуенко <nikolajduenko@MacBook-Air-Nikolaj.local>
2025-06-13 00:11:51 +0300

    🔧 Optimize Vite config for iOS caching issues

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    assetsInlineLimit: 0, // отключает инлайнинг больших файлов
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]', // добавляет хеш к файлам
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
+});
