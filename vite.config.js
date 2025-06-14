fa79af78fc28fa564cc9d284261ac1de0cd1c185
Николай Дуенко <nikolajduenko@MacBook-Air-Nikolaj.local>
2025-06-14 13:36:38 +0300

    init deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/jeeping-vardane/',
   plugins: [react()],
-  build: {
-    assetsInlineLimit: 0,
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash][extname]',
-      },
-    },
-  },
 })
+
