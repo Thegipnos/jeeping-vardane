ac588ede6c7df3d0708f28b2f35734509de239f4
Николай Дуенко <nikolajduenko@MacBook-Air-Nikolaj.local>
2025-06-14 13:06:48 +0300

    ♻️ Fix MIME type issues for Netlify/Vercel mobile

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // важно для Netlify, если корневая директория
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash][extname]',
+      },
+    },
+  },
 })
